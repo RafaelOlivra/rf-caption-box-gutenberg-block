4534c36c4e8232b1dbe7768fb179e0de9996bdfa
Rafael Oliveira <contact@rafaeloliveiradesign.com>
2025-04-09 16:29:17 -0300

    Updated build

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '9431b082dbde87ba2b7b');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '149447f539b1240cd506');
